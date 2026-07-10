a5704d106b81a6096b4c787097d2312154bca994
Richard Davisson <71906941+Chessgeek-svg@users.noreply.github.com>
2026-07-10 12:16:58 -0700

    Add: coin-change-ii - submission-4

---
+++ Data Structures & Algorithms/coin-change-ii/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins)):
+            for a in range(coins[i], amount + 1):
+                dp[a] += dp[a-coins[i]]
+        return dp[amount]
